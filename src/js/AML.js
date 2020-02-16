835ed4bf6663a227fb730fe10f6b8cb1b3e255b3
John Erfurt <erfurtjohn@gmail.com>
2020-02-16 09:46:38 +0100

    changed example data

--- src/js/AML.js
+++ src/js/AML.js
@@ -85,22 +85,38 @@ class AML {
             {
                 location_latitude: 51.3512,
                 location_longitude: 7.445,
-                location_time: "2019-10-17 09:27:29"
+                location_time: "2019-10-17 09:27:29",
+                location_accuracy: "15.515",
+                location_vertical_accuracy: "2.5",
+                location_altitude: "160.5",
+                location_source: "wifi",
             },
             {
                 location_latitude: 51.4028,
                 location_longitude: 7.4758,
-                location_time: "2019-10-17 09:27:29"
+                location_time: "2019-10-17 09:27:29",
+                location_accuracy: "15.515",
+                location_vertical_accuracy: "2.5",
+                location_altitude: "160.5",
+                location_source: "wifi",
             },
             {
                 location_latitude: 51.3849,
                 location_longitude: 7.4794,
-                location_time: "2019-10-17 09:27:29"
+                location_time: "2019-10-17 09:27:29",
+                location_accuracy: "15.515",
+                location_vertical_accuracy: "2.5",
+                location_altitude: "160.5",
+                location_source: "wifi"
             },
             {
                 location_latitude: 51.3555,
                 location_longitude: 7.4722,
-                location_time: "2019-10-17 09:27:29"
+                location_time: "2019-10-17 09:27:29",
+                location_accuracy: "15.515",
+                location_vertical_accuracy: "2.5",
+                location_altitude: "160.5",
+                location_source: "wifi",
             }
         ];
     }
